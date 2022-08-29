4ed1e7254fd080063eceadf71712da00c8988334
Sachin Shukla <91952010+Sachin4219@users.noreply.github.com>
2022-08-29 19:28:46 +0530

    Add files via upload

---
+++ models/complaint.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const complaintSchema = mongoose.Schema({
+    name:{type:String, required:true},
+    hostelName:{type:String, required:true},
+    issueCategory:{type:String, required:true},
+    availiability:{type:String, required:true},
+    phoneNumber:{type:String, required:true},
+    description:String,
+    handlingOfficer:Number,
+    date:Date,
+    status:String
+})
+
+const Complaint = mongoose.model("Complaint", complaintSchema)
+
+export default Complaint
